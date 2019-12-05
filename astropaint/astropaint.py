fcbfd2c5afd69f6a9df4c0d4b30e3294afe5dd3e
Siavash Yasini <yasini@usc.edu>
2019-12-04 18:10:06 -0800

    Add test box generator to Catalog.

--- astropaint/astropaint.py
+++ astropaint/astropaint.py
@@ -258,21 +258,38 @@ class Catalog:
         return pd.DataFrame(catalog)  # convert catalog to pandas data frame
 
     @staticmethod
-    def generate_test_box(configuration="center",
+    def generate_test_box(configuration=["front"],
                           distance=100,
-                          mass=1E14,
+                          mass=1E15,
                           ):
 
-        if configuration is "center":
-            # generate single test halo
-            catalog = Catalog._initialize_catalog(1)
+        catalog = pd.DataFrame(Catalog._initialize_catalog(0))
+        config_dict = {"front": (1, 0, 0),
+                       "back": (-1, 0, 0),
+                       "left": (0, 1, 0),
+                       "right": (0, -1, 0),
+                       "top": (0, 0, 1),
+                       "bottom": (0, 0, -1),
+                       }
 
-            catalog["x"], catalog["y"], catalog["z"] = (distance, 0, 0)
+        # set configuration for "all" keyword
+        if "all" in configuration:
+            configuration = config_dict.keys()
 
-            # generate random log uniform masses
-            catalog["M_200c"] = mass
+        for key in configuration:
+            # get the coordinates from config_dic and load it in a dataframe
+            x, y, z = config_dict[key]
+            df = pd.DataFrame(Catalog._initialize_catalog(1))
+            df["x"], df["y"], df["z"] = x, y, z
+            df[["x", "y", "z"]] *= distance
 
-        return pd.DataFrame(catalog)  # convert catalog to pandas data frame
+            # set the mass
+            df["M_200c"] = mass
+
+            # append the test case to the catalog
+            catalog = catalog.append(df, ignore_index=True)
+
+        return pd.DataFrame(catalog)  # return the pandas dataframe
 
     @staticmethod
     def _set_octant(df, octant):
