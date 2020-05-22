605fcb60754d79558bcbcca3239a8656b40952b1
Siavash Yasini <yasini@usc.edu>
2020-05-21 18:50:42 -0700

    Rename to paint_bucket

--- astropaint/__init__.py
+++ astropaint/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
 
-from .astropaint import Catalog, Canvas, Painter
+from .paint_bucket import Catalog, Canvas, Painter
 from .lib import transform, utilities
 from .lib.utilities import LOS_integrate, interpolate
