38828c2e913146e358279619839460644101cf17
syasini <siavash.yasini@gmail.com>
2020-07-20 14:53:11 -0400

    Fix bugs and update methods
    change file name to datasets.py
    rewrite download method to avoid code repetition
    fix typo in WebSky_2x2 name
    fix bug in file save location

--- astropaint/lib/datasets.py
+++ astropaint/lib/datasets.py
@@ -4,7 +4,7 @@ __author__ = "Siavash Yasini"
 __email__ = "yasini@usc.edu"
 
 import requests
-from tqdm.auto import tqdm
+from tqdm import tqdm
 import os
 
 
@@ -14,54 +14,61 @@ import os
 #########################################################
 
 
-def _get_dataset(ds_name, ds_url):
+def _get_dataset(file_location, ds_url, dataset_name=None):
     """
         @purpose: Connect and Download the necessary files
         @input:
-            ds_name: str: DataSet Names
+            file_location: str: file location to download dataset
             ds_url: str: link to the datasets
+            dataset_name: str: name of the dataset
 
     """
     response = requests.get(ds_url, stream=True)
 
-    with tqdm.wrapattr(open(f'{ds_name}.csv', "w"), "write", miniters=1,
+    if dataset_name is None:
+        dataset_name = file_location
+
+    with tqdm.wrapattr(open(file_location, "wb"), "write", miniters=1,
                        total=int(response.headers.get('content-length', 0)),
-                       desc=ds_name) as fout:
+                       desc=dataset_name) as fout:
         for chunk in response.iter_content(chunk_size=4096):
             fout.write(chunk)
 
 
-def download_dataset(dataset_name:str='All'):
+def download(dataset_name:str='All'):
     """
         @purpose: Download datasets mannualy if not exists into data folder.
         @Input: Input a string:
                 "All" : Downloads all the relevant datasets.
                 "WebSky_lite" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_lite.csv', 
-                "WebSky_2x2" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_2x2111.csv',
+                "WebSky_2x2" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_2x2.csv',
                 "Sehgal" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/Sehgal.csv',
                 "MICE" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/MICE.csv'
 
     
     """
-    dict_datasets = {
+    datasets_url_dict = {
         "WebSky_lite" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_lite.csv', 
-        "WebSky_2x2" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_2x2111.csv',
+        "WebSky_2x2" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/WebSky_2x2.csv',
         "Sehgal" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/Sehgal.csv',
-        "MICE" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/MICE.csv'
+        "MICE" : 'https://media.githubusercontent.com/media/syasini/AstroPaint/master/astropaint/data/MICE.csv',
     }
-    
+
+    # make a list of all the datasets to be downloaded
+    download_list = []
+
     if dataset_name == 'All':
-        for ds_name, ds_url in dict_datasets.items():
-            file_location = os.path.join(os.path.dirname(__file__), os.path.pardir,'data',f'{ds_name}.csv')
-            if ~ os.path.exists(file_location):
-                _get_dataset(ds_name,ds_url)
-            else:
-                print(f'{ds_name} already exists!')
-    else:
-        file_location = os.path.join(os.path.dirname(__file__), os.path.pardir,'data',f'{dataset_name}.csv')
-        if ~ os.path.exists(file_location):
-            _get_dataset(ds_name,ds_url, file_location)
-        else:
-            print(f'{dataset_name} already exists!')
-    print('END!')
+        download_list = [ds_name for ds_name in datasets_url_dict.keys()]
+    elif type(dataset_name) == list:
+        download_list = dataset_name
+    elif type(dataset_name) == str:
+        download_list.append(dataset_name)
+    # TODO: add an else statement here to raise an error 
+
+    for dataset in download_list:
+        file_location = os.path.join(os.path.dirname(__file__), os.path.pardir,'data',f'{dataset}.csv')
+        _get_dataset(file_location, datasets_url_dict[dataset], dataset)
+    #TODO: add overwrite=True arg to the function 
+
+    print('Done!')
 
